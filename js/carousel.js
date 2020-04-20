53b36cb68f0a9a7794a17f3283997b309d9910d0
A.Rupesh <a.rupesh66@gmail.com>
2020-04-21 04:28:58 +0530

    :wastebasket: old files

---
+++ js/carousel.js
@@ -0,0 +1,16 @@
+//
+//  Portfolio
+//  carousel.js
+//
+//  Created By Ryan Maugin on 06/05/17
+//
+
+$(document).ready(function () {
+    
+    $('.experiences-carousel').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+    });
+
+});
